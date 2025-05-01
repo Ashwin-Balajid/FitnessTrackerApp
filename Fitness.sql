c0a8e6c78e3d728cf2e428aacbb0f270b1b3e3ef
Ashwin-Balajid <141709172+Ashwin-Balajid@users.noreply.github.com>
2025-05-01 09:57:50 +0530

    Add files via upload

---
+++ Fitness.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE fitness;
+use  fitness;
+show tables;
+select * from fitness_calorie;
+select * from fitness_workout;
+select * from fitness_goal;
+select * from fitness_bmi;
+select * from auth_user;
+select * from django_admin_log;
+select * from auth_permission;
+
